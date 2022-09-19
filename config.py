213186c3e80a3262ce7f726a931899ab0351317b
jiaaaxuannn <98567413+jiaaaxuannn@users.noreply.github.com>
2022-09-19 16:04:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cyck8kqhbfrs.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chongjiaxuan-employee"
+customregion = "us-east-1"
